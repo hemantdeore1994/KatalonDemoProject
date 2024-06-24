f18903666b7c3549e1b36e5c577b3abd9e5600da
hemant <deore.hemant1994@gmail.com>
2024-06-24 17:53:00 +0530

    testcase removed

--- Test Suites/TestSuiteDemo1.ts
+++ Test Suites/TestSuiteDemo1.ts
@@ -12,24 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>f07bb31c-7a39-47c7-995d-141c41f842ae</testSuiteGuid>
    <testCaseLink>
-      <guid>2540269c-ab0e-47d3-8c35-d3f875e7597a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OrangeHrmLogin_ByRecord_Test</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>acb4d6ed-24c4-476e-9f3e-ec94728a0cd2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/RedbusTestCase</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>328109e1-15dd-49b0-8886-d78861f6d6a9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OrangeHrmLogin_ByRecord_Test</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/TestSuiteDemo2.ts
+++ Test Suites/TestSuiteDemo2.ts
@@ -12,10 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>be1e5be7-1d2d-4852-b8ef-5d449aea576f</testSuiteGuid>
    <testCaseLink>
-      <guid>c6abb70f-90d7-4996-b052-2a42fee59060</guid>
+      <guid>be7ea047-0bc0-4424-bc3b-4c251405db33</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/OrangeHrmLogin_ByRecord_Test</testCaseId>
+      <testCaseId>Test Cases/DemogithubTestCase</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
